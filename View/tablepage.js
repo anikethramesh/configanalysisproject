19b065e7d66193aab9ec1deea00697e02b547224
Aniketh Ramesh <aniketh.ramesh@ericsson.com>
2017-05-17 15:41:24 +0530

    JSONP Change

--- View/tablepage.js
+++ View/tablepage.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("#hello").text("Blah blah");
 	$('#table').bootstrapTable({
 	    url: 'http://localhost:5000/lr2get',
+	    dataType: "jsonp",
 	    headers: {
             'Access-Control-Allow-Origin': '*'
         },
